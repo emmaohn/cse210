1b2ce3f47fe12bc3346cccf614042fcfac1dda1e
emmaohn <joh20123@byui.edu>
2023-01-07 22:28:07 -0600

    prep 1 and prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage? ");
+        string userValue = Console.ReadLine();
+        int grade = int.Parse(userValue);
+        
+        string letter;
+        string message;
+
+        if (grade >= 90) {
+            letter = "A";
+        } else if (grade >= 80) {
+            letter = "B";
+        } else if (grade >= 70) {
+            letter = "C";
+        } else if (grade >= 60) {
+            letter = "D";
+        } else {
+            letter = "F";
+        }
+        Console.WriteLine($"{letter}");
+
+        if (grade >= 70) {
+            message = "Great Job!";
+        } else {
+            message = "Keep focusing!";
+        }
+        Console.WriteLine($"{message}");
     }
 }
