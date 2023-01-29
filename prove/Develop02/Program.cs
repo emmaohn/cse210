a24d720382b73533a0d4fba0e41a7c7cc7df8855
emmaohn <joh20123@byui.edu>
2023-01-28 20:48:35 -0600

    formatting develop 02

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -14,7 +14,7 @@ class Program
         
         while (userChoice != "5") {
             // show option menu
-            Console.WriteLine("Please select one of the following choices\n1. Write\n2. Display\n3. Load\n4. Save\n5. Quit\nWhat would you like to do?");
+            Console.WriteLine("\nPlease select one of the following choices\n1. Write\n2. Display\n3. Load\n4. Save\n5. Quit\nWhat would you like to do?");
             userChoice = Console.ReadLine();
 
             if (userChoice == "1") {
@@ -29,6 +29,12 @@ class Program
                 string userInput = Console.ReadLine();
                 // create an entry and store it locally
                 journal.CreateEntry(currentDate, prompt, userInput);
+                // Color tells user the entry ahs been created and prompts a save
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nEntry created", Console.ForegroundColor);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Press 4 to save entry", Console.ForegroundColor);
+                Console.ResetColor();
 
             } else if (userChoice == "2") {
                 // display!!!!!
@@ -36,23 +42,36 @@ class Program
 
             } else if (userChoice == "3") {
                 // load file
-                Console.WriteLine("What file would you like to load?");
+                Console.WriteLine("\nWhat file would you like to load?");
                 string fileName = Console.ReadLine();
                 journal.LoadJournal(fileName);
+                // Color tells the user the file that has been loaded
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"{fileName} has been loaded", Console.ForegroundColor);
+                Console.ResetColor();
 
             } else if (userChoice == "4") {
                 // save to a file, overwrite if file already exists
-                Console.WriteLine("Where would you like to store this journal?");
+                Console.WriteLine("\nWhere would you like to store this journal?");
                 string fileName = Console.ReadLine();
                 journal.SaveJournal(fileName);
+                // Color tells the user if the file has been saved
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"File saved to '{fileName}'", Console.ForegroundColor);
+                Console.ResetColor();
 
             } else if (userChoice == "5") {
                 // bye bye code
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("\nGoodbye!", Console.ForegroundColor);
+                Console.ResetColor();
                 break;
 
             } else {
                 // if there is no valid input, run the while again
-                Console.WriteLine("Please enter a valid menu number");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nPlease enter a valid menu number", Console.ForegroundColor);
+                Console.ResetColor();
             }
         }
     }
