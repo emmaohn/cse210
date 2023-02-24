dd7dac8a7203e7bda5ea2d3b4dd60efcf698f61c
emmaohn <joh20123@byui.edu>
2023-02-23 19:23:55 -0600

    learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("jojo", "stuff and things");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Emma", "Nultiplication", "7.3", "8-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Levi", "European history", "Causes of world war two");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
