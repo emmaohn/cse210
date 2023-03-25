1098b274ee706fc8c6224803d0f79b46d3a6a3c2
emmaohn <joh20123@byui.edu>
2023-03-24 20:59:22 -0500

    Foundations 1 and 2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Console.WriteLine("Hello Foundation1 World!");
+        List<Video> _videos = new List<Video>();
+
+        // video 1
+        Video video1 = new Video("Cardinals in the snow", "Birds of Indiana", 303);
+        video1.comment("George335", "Nice Orioles!");
+        video1.comment("Emmett4evA", "Pretty birbs");
+        video1.comment("EmmaForBirds00", "More hummingbirds?");
+        _videos.Add(video1);
+
+        Video video2 = new Video("Some birds but mostly a chipmunk", "Chipmunks of Indiana", 793);
+        video2.comment("LeviW", "Haha, his cheeks so full hehe");
+        video2.comment("EmmaForBirds00", "More Birds pls");
+        video2.comment("Jas0n024", "Oh, yeah! Good stuff");
+        _videos.Add(video2);
+
+        Video video3 = new Video("Blue jays in Indiana", "Indiana Blue Jays", 1107);
+        video3.comment("GrantNTruds", "Sweet birds! We love when the geese come to our pond for the winter!");
+        video3.comment("Derek55", "I once saw a bird--great stuff");
+        video3.comment("Russ", "Generally I prefer eagles, but these are some nice birds");
+        _videos.Add(video3);
+
+        foreach(Video video in _videos) {
+            video.Print();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Console.WriteLine("Hello Foundation2 World!");
+
+        // Make sure that all member variables are private and getters, setters, and constructors are created as needed.
+
+        // Once you have created these classes, write a program that creates at least two orders with a 2-3 products each. Call the methods to get the packing label, the shipping label, and the total price of the order, and display the results of these methods.
+
+        Product product1 = new Product("Book", "8937", 10.99, 2);
+        Product product2 = new Product("Back pack", "2464", 20.65, 5);
+        Product product3 = new Product("Unicycle", "4629", 140.34, 1);
+
+        Order order1 = new Order();
+        Address address1 = new Address("238 Leaf Trl.", "Dallas", "TX", "USA");
+        Customer customer1 = new Customer("Emma Johnson", address1);
+        order1.AddItem(product1);
+        order1.AddItem(product3);
+        // order1.PrintAllCosts(address1);
+        order1.PrintTotalCost(address1);
+        order1.PrintPackingLabel();
+        order1.PrintShippingLabel(address1, customer1);
+        Console.WriteLine();
+
+        Order order2 = new Order();
+        Address address2 = new Address("324 Water Ave.", "Amsterdam", "North Holland", "Netherlands");
+        Customer customer2 = new Customer("Levi Jones", address2);
+        order2.AddItem(product2);
+        order2.AddItem(product3);
+        // order2.PrintAllCosts(address2);
+        order2.PrintTotalCost(address2);
+        order2.PrintPackingLabel();
+        order2.PrintShippingLabel(address2, customer2);
+
     }
 }
