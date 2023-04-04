f27529a14cf7b381fb3aae6922d10558ea9ec969
emmaohn <joh20123@byui.edu>
2023-04-04 09:47:28 -0500

    Final Foundations 3 and 4

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        // Console.WriteLine("Hello Foundation3 World!");
+
+        // title, description, date, time, address, speaker, capacity
+        Lecture lecture = new Lecture("Birds of Indiana", "Levi William recounts his most interesting enounters with the birds of indiana", "03/30/2023", "11:00pm-12:00pm", new Address("3726 Trail trl", "Fort Wayne", "VA"), "Levi William", 110);
+        lecture.StandardDetails();
+        Console.WriteLine();
+        lecture.FullDetails();
+        Console.WriteLine();
+        lecture.ShortDescription();
+        Console.WriteLine();
+
+        Reception reception = new Reception("Regina and Josh's Wedding Reception", "Immediately following a wedding ceremony, a reception will be held for Josh and Regina", "04/27/2025", "1pm-4pm", new Address("3820 Jershua Ave.", "Galveston", "TX"), "reservations@rsvp.com");
+        reception.StandardDetails();
+        Console.WriteLine();
+        reception.FullDetails();
+        Console.WriteLine();
+        reception.ShortDescription();
+        Console.WriteLine();
+
+        OutdoorGathering outdoorGathering = new OutdoorGathering("Grace Dance Studio Recital", "A small recital will be held on an outdoor stage for parents", "08/24/2023", "9am-10am", new Address("932 West st.", "Weston", "OK"), "Partly cloudy, 78º f");
+        outdoorGathering.StandardDetails();
+        Console.WriteLine();
+        outdoorGathering.FullDetails();
+        Console.WriteLine();
+        outdoorGathering.ShortDescription();
+        Console.WriteLine();
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        // Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        DateTime currentTime =  DateTime.Now;
+        string currentDate =  currentTime.ToShortDateString();
+
+        activities.Add(new Run(currentDate, 30, 3));
+        activities.Add(new Bike(currentDate, 20, 7));
+        activities.Add(new Swim(currentDate, 25, 8));
+
+        foreach (Activity activity in activities) {
+            activity.GetSummary();
+        }
     }
 }
